3f476345639f87dac8d0e88308158e1964b3c9a5
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-04-24 15:53:19 -0500

    Se completa funcionalidad principal, solo flata agregar paginación y refactorizar viewmodels

--- CrudPeliculas_RazorPages_EntityFramework/Pages/Index.cshtml.cs
+++ CrudPeliculas_RazorPages_EntityFramework/Pages/Index.cshtml.cs
@@ -13,29 +13,30 @@ namespace CrudPeliculas_RazorPages_EntityFramework.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        public List<Pelicula> peliculas = new List<Pelicula>();
+        public List<Pelicula> Peliculas = new List<Pelicula>();        
+        private readonly dbPeliculasContext _dbPeliculasContext;
+
         [BindProperty]
         public ViewModelPelicula Pelicula { get; set; }
 
-        private readonly dbPeliculasContext _dbPeliculasContext;
+
         public IndexModel(ILogger<IndexModel> logger, dbPeliculasContext pdbPeliculasContext)
         {
             _logger = logger;
             _dbPeliculasContext = pdbPeliculasContext;
-
 
         }
 
         public void OnGet()
         {
 
-            peliculas = _dbPeliculasContext.Peliculas.ToList();
+            Peliculas = _dbPeliculasContext.Peliculas.ToList();
             ViewData["Accion"] = "Listar";
 
         }
         public void OnGetEditar(int? id)
         {
-
+            ViewData["Accion"] = "Editar";
             var pel = _dbPeliculasContext.Peliculas.Single(p => p.Id == id);
 
             Pelicula = new ViewModelPelicula() { Duracion = pel.Duracion,
@@ -51,6 +52,18 @@ namespace CrudPeliculas_RazorPages_EntityFramework.Pages
 
         }
 
+        public void OnGetElimiar(int? id)
+        {
+            var pelicula = _dbPeliculasContext.Peliculas.FirstOrDefault(p => p.Id == id);
+            _dbPeliculasContext.Peliculas.Remove(pelicula);
+            _dbPeliculasContext.SaveChanges();
+            ViewData["Accion"] = "Listar";
+            Peliculas = _dbPeliculasContext.Peliculas.ToList();
+          
+
+        }
+
+
         public void OnPostCrear()
         {
 
@@ -59,7 +72,7 @@ namespace CrudPeliculas_RazorPages_EntityFramework.Pages
                 var pelicula = new Pelicula() { Titulo = Pelicula.Titulo, Duracion = Pelicula.Duracion };
                 _dbPeliculasContext.Peliculas.Add(pelicula);
                 _dbPeliculasContext.SaveChanges();
-                peliculas = _dbPeliculasContext.Peliculas.ToList();
+                Peliculas = _dbPeliculasContext.Peliculas.ToList();
                 ViewData["Accion"] = "Listar";
             }
             else
@@ -68,6 +81,29 @@ namespace CrudPeliculas_RazorPages_EntityFramework.Pages
                 ViewData["Accion"] = "Nuevo";
             }
             
+
+        }
+
+        public void OnPostEditar()
+        {
+
+            if (ModelState.IsValid)
+            {
+                var pelicula = _dbPeliculasContext.Peliculas.SingleOrDefault(p => p.Id == Pelicula.Id);
+
+                pelicula.Titulo = Pelicula.Titulo;
+                pelicula.Duracion = Pelicula.Duracion;
+                
+                _dbPeliculasContext.SaveChanges();
+                Peliculas = _dbPeliculasContext.Peliculas.ToList();
+                ViewData["Accion"] = "Listar";
+            }
+            else
+            {
+
+                ViewData["Accion"] = "Editar";
+            }
+
 
         }
     }
